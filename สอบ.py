9c393718af29329b837765ea6a90b783cc7ca1cc
kannisa1234 <kannisa6593@gmail.com>
2024-12-23 14:59:18 +0700

    Add files via upload

---
+++ สอบ.py
@@ -0,0 +1,20 @@
+x = input()
+n = int(input())
+Z = (x*n)
+print(Z)
+
+
+
+midterm = float(input())
+final = float(input())
+print("Total:",midterm + final )
+print("Average:",total / 2 )
+
+
+
+ความกว้าง=float(input())
+ความยาว=float(input())
+ความลึก=float(input())
+ปริมาตร=ความกว้าง*ความยาว*ความลึก
+ต้องใช้เวลา=(ปริมาตร/0.25)
+print(ต้องใช้เวลา)
